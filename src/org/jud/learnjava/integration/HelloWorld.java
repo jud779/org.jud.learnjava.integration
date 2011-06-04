5740fc663a62a01ead653e02ed8e65294948445b
jud779 <jud779@gmail.com>
2011-06-04 14:41:21 -0400

    Java integration project first commit.

---
+++ src/org/jud/learnjava/integration/HelloWorld.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package org.jud.learnjava.integration;
+
+/**
+ * @author Janak
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World!");
+
+	}
+
+}
